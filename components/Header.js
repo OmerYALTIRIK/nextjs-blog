8971eb2ce847c4d6edeaed786710012ad9a68716
Ömer Yaltırık <40443652+OmerYALTIRIK@users.noreply.github.com>
2023-04-05 01:03:36 +0300

    Styled Comp kuruldu ve düzenlemeleri yapıldı

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const HeaderDiv = styled.div`
+  color: #f00;
+  border: 10px solid #f00;
+  padding: 30px;
+`;
+
+export default () => (
+  <div>
+    <HeaderDiv>
+      <Link href="/">Anasayfa</Link>
+    </HeaderDiv>
+    <div>
+      <Link href="/about">Hakkımızda</Link>
+    </div>
+  </div>
+);
---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Header from "./Header";
+import styled from "styled-components";
+
+const StyledHeader = styled(Header)`
+  border: 10px solid yellow;
+  backgroundColor: red;
+`;
+export default ({ title, children }) => (
+  <div>
+    <h1>{title}</h1>
+    <StyledHeader />
+    <div style={{ padding: "20px", margin: "20px", backgroundColor: "tomato" }}>{children}</div>
+    <footer>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by vercel
+      </a>
+    </footer>
+  </div>
+);
