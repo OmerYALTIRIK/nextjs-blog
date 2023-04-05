0f2202e99db57996d2495cf58ecffbe13e530662
Ömer Yaltırık <40443652+OmerYALTIRIK@users.noreply.github.com>
2023-04-05 20:52:26 +0300

    next upgrade

--- components/Layout.js
+++ components/Layout.js
@@ -5,12 +5,25 @@ const BodyDiv = styled.div`
   margin: 0;
   padding: 0;
 `;
+
+const Footer = styled.footer`
+  padding: 20px;
+  background-color: #ccc;
+`;
+
+const Content = styled.div`
+  padding: 20px;
+  background-color: #f00;
+`;
+
 export default ({ title, children }) => (
   <BodyDiv>
     <h1>{title}</h1>
     <Header />
     <div style={{ padding: "20px", margin: "20px", backgroundColor: "tomato" }}>{children}</div>
-    <footer>
+
+    <Content>{children}</Content>
+    <Footer>
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
@@ -18,6 +31,6 @@ export default ({ title, children }) => (
       >
         Powered by vercel
       </a>
-    </footer>
+    </Footer>
   </BodyDiv>
 );
